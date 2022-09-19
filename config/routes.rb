b614fbc22984b66feb44f972f2820e3865273502
Emmanuel Paul <emmapaul247@gmail.com>
2022-09-19 13:32:12 +0100

    Create models according to ERD diagram

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/index'
+  # mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'home#index'
+  root 'pages#index'
 end
